7c9e052300eaf897c34cbaa4f769c1f88e583740
Ashish Maheshwari <maheshwari.ashish1992@gmail.com>
2015-02-11 18:19:07 +0530

    minor change to quick sort

--- Quick sort.cpp
+++ Quick sort.cpp
@@ -17,28 +17,26 @@ using namespace std;
 
 int partition(int*, int, int);
 void swap(int*, int, int);
-void printArr(int*, int);
 
 void quickSort(int* arr, int lo, int hi) {
-	if (lo < hi) {
-		int p = partition(arr, lo, hi);
-		quickSort(arr, lo, p-1);
-		quickSort(arr, p+1, hi);
-	}
+	if (lo > hi)
+		return;
+	int pivot = partition(arr, lo, hi);
+	quickSort(arr, lo, pivot-1);
+	quickSort(arr, pivot+1, hi);
 	return;
 }
 
 int partition(int* arr, int lo, int hi) {
-	int pivotIndex = lo, pivotValue = arr[pivotIndex];
-	int storeIndex = lo;
-	swap(arr, pivotIndex, hi);
-	for (int i=lo; i<hi; i++)
-		if (arr[i] < pivotValue) {
-			swap(arr, i, storeIndex);
-			++storeIndex;
+	int x = arr[hi];
+	int pivotIndex = lo;
+	for (int j=lo; j<hi; j++)
+		if (arr[j] < x) {
+			swap(arr, j, pivotIndex);
+			pivotIndex++;
 		}
-	swap(arr, storeIndex, hi);
-	return storeIndex;
+	swap(arr, pivotIndex, hi);
+	return pivotIndex;
 }
 
 void swap(int* arr, int a, int b) {
@@ -49,9 +47,9 @@ void swap(int* arr, int a, int b) {
 }
 
 void printArr(int* arr, int size) {
-    for(int i=0; i<size; i++)
-        cout<<arr[i]<<endl;
-    return;
+	for (int i=0; i<size; i++)
+		cout<<arr[i]<<" ";
+	return;
 }
 
 int main() {
@@ -62,8 +60,9 @@ int main() {
     int arr[size];
     for(int i=0; i<size; i++)
     	cin>>arr[i];
-    cout<<"Quick sort:"<<endl;
+    cout<<"Quick sort.."<<endl;
     quickSort(arr, 0, size-1);
     printArr(arr, size);
+    cout<<endl;
     return 0;
 }
