0dfb5b892ea7d136f2611229b2a79469f417a3d8
Ashish Maheshwari <maheshwari.ashish1992@gmail.com>
2015-03-05 18:49:09 +0530

    added stable and unstable method to counting sort

--- counting-sort.cpp
+++ counting-sort.cpp
@@ -6,22 +6,37 @@ void countingSort(int* A, int n, int k);
 void printArr(int* arr, int size);
 
 void countingSort(int* A, int n, int k) {
-	int B[k], C[n];
+	// B1 is used for stable and B2 for unstable temp storage
+	// C is used as output array
+	int B1[k], B2[k], C[n];
 	for (int i = 0; i < k; i++)
-		B[i] = 0;
-	for (int i = 0; i < n; i++)
-		B[A[i]] = B[A[i]] + 1;
+		B1[i] = B2[i] = 0;
+	for (int i = 0; i < n; i++) {
+		B1[A[i]] = B1[A[i]] + 1;
+		B2[A[i]] = B2[A[i]] + 1;
+	}
 	// B[i] now contains the number of elements equal to i
 
-	for (int i = 1; i < n; i++)
-		B[i] = B[i] + B[i-1];
+	for (int i = 1; i < n; i++) {
+		B1[i] = B1[i] + B1[i-1];
+		B2[i] = B2[i] + B2[i-1];
+	}
 	// B[i] now contains the number of elements ≤ i
 
-	for (int i = 0; i < n; i++) {
-		C[B[A[i]]-1] = A[i];
-		--B[A[i]];
+	// reading A from index n-1 results in stable sort
+	for (int i = n-1; i >= 0; i--) {
+		C[B1[A[i]]-1] = A[i];
+		--B1[A[i]];
 	}
-	cout<<endl<<"counting sort: ";
+	cout<<endl<<"counting sort (stable): ";
+	printArr(C, n);
+
+	// reading A from index 0 results in unstable sort
+	for (int i = 0; i < n; i++) {
+		C[B2[A[i]]-1] = A[i];
+		--B2[A[i]];
+	}
+	cout<<endl<<"counting sort (unstable): ";
 	printArr(C, n);
 }
 
